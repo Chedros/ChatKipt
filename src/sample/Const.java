83a97eedc18f578da1ae775a4ab2d60e0c78cad1
Chedros <48429899+Chedros@users.noreply.github.com>
2019-11-06 11:22:01 +0200

    ADD DB conn

---
+++ src/sample/Const.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Const {
+    public static final String CONNECTION_URL = "jdbc:postgresql://igncurjn:ZNiqacxU02o1iIXXCWAjy4NOdzRxGoRQ";
+    public static final String DB_USER = "igncurjn";
+    public static final String DB_PASSWORD = "ZNiqacxU02o1iIXXCWAjy4NOdzRxGoRQ";
+
+    public static final String USER_TABLE = "users";
+
+
+
+}
---
+++ src/sample/DataBase.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBase {
+    private static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(Const.CONNECTION_URL,Const.DB_USER ,Const.DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
+
+
+}
