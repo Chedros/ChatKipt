a52d19078279b99d7186b6066d4d65e23ceaef5d
Chedros <48429899+Chedros@users.noreply.github.com>
2019-11-10 17:23:37 +0200

    Add start menu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("ChatKipt");
+        primaryStage.setScene(new Scene(root, 745, 437));
         primaryStage.show();
     }
 
